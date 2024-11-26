cad57dc8d93db6eeb956b5a76e333c59caaf3277
DeringSalazar <desalazarsa@est.utn.ac.cr>
2024-11-26 08:25:39 -0600

    Inicio del examen

---
+++ examen/pkg2/Examen2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen.pkg2;
+
+public class Examen2 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
